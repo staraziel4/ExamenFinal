0c4b8e1c1207cb0d7465ac53dec4701e63de9f2f
raziel <raziel.ascui@uab.edu.bo>
2023-12-04 16:21:38 +0100

    subido del modelado

---
+++ ProyectoBackendRaziel/WebApplication2/Models/Aviones.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Modelss
+{
+    public class Aviones
+    {
+        [Key]
+        public int id_aviones { get; set; }
+        public string modelo { get; set; }
+        public int peso { get; set; }
+        public int capacidad { get; set; }
+        public int id_piloto { get; set; }
+        public int id_hangares { get; set; }
+       
+    }
+}
---
+++ ProyectoBackendRaziel/WebApplication2/Models/Piloto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Modelsss
+{
+    public class Piloto
+    {
+        [Key]
+        public int id_piloto { get; set; }
+        public string restricciones { get; set; }
+        public int salario { get; set; }
+        public string turno { get; set; }
+
+
+    }
+}
